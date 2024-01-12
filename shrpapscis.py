aa9d38c4567fe56071499a9a538c2ed645c8d52e
Singhumd <154690660+Singhumd@users.noreply.github.com>
2024-01-12 21:53:59 +0530

    shrpapscis.py

--- shrpapscis.py
+++ shrpapscis.py
@@ -4,8 +4,25 @@ import random
 print ("\n welcome to rock ,paper , sessisor game")
 print("\n")
 
-user_input = input("Enter a choice (rock, paper, scissors): ")
-possible_actions = ("rock", "paper", "scissors")
-computer_input = random.choice(possible_actions)
-print("\n You choose")
-    
+userinput = input("Enter a choice (rock, paper, scissor): ")
+possible_actions = ("rock", "paper", "scissor")
+computerinput = random.choice(possible_actions)
+print(f"\nYou chose {userinput}, computer chose {computerinput}.\n")
+
+if userinput == computerinput:
+    print(f"Both players selected {userinput}. It's a tie!")
+elif userinput == "rock":
+    if computerinput == "scissor":
+        print("Rock beats scissor! You win!")
+    else:
+        print("Paper beats rock! You lose.")
+elif userinput == "paper":
+    if computerinput == "rock":
+        print("Paper beats rock! You win!")
+    else:
+        print("Scissors beats paper! You lose.")
+elif userinput == "scissor":
+    if computerinput == "paper":
+        print("Scissor beats paper! You win!")
+    else:
+        print("Rock beats scissor! You lose.")
